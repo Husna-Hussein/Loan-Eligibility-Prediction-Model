a60a662d1e80cb61d7a275abeb07b67d45dcd00c
Husna Hussein <husnahussainkilalo@gmail.com>
2023-05-18 14:09:39 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    if prediction[0]== 0:
+        output ="Not Eligible"
+    elif prediction[0]== 1:
+        output = "Eligible"
+
+    return render_template('index.html', prediction_text=f'{output}')
+
+@app.route('/results',methods=['POST'])
+def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
